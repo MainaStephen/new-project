ece94301c796fec462b1417f3eb49360a6a19c97
stephen <mainastephen159@gmail.com>
2023-02-15 18:12:35 +0300

    first commit

---
+++ dart/assignment.dart
@@ -0,0 +1,1 @@
+//Assignment: create  a calculator that allows users to input their values and perform an arithmetic operation
---
+++ dart/main.dart
@@ -0,0 +1,8 @@
+void main() {
+  var student ="Jacob";
+  var country ="south africa";
+  var phone = 0717220807;
+  var height = 5.6;
+  var marital = "married";
+  print("my name is $student.i come from $country and my phone number is $phone");
+}
