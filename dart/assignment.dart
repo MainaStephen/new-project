9989de1bc7d8e86993d25d9c8ab4df0e86826679
stephen <mainastephen159@gmail.com>
2023-02-23 17:18:09 +0300

    update

--- dart/assignment.dart
+++ dart/assignment.dart
@@ -1,1 +1,34 @@
-//Assignment: create  a calculator that allows users to input their values and perform an arithmetic operation
+import 'dart:io';
+
+void main() {
+  print('Enter first number: ');
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  print('Enter second number: ');
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  print('Enter operation (+, -, *, /): ');
+  String operation = stdin.readLineSync()!;
+
+  double result = 0;
+
+  switch (operation) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      result = num1 / num2;
+      break;
+    default:
+      print('Invalid operation!');
+      return;
+  }
+
+  print('Result: $result');
+}
---
+++ dart/jambo.dart
@@ -0,0 +1,6 @@
+import 'dart:io';
+
+void main() {
+  print ("enter marks attained");
+  int mark = int.parse(stdin.readLineSync()!);
+}
